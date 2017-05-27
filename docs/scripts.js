b710fe5ff62ebac2a864d82119cc18173c65e774
Theodore Ricketts <trk@clarksons.co.uk>
2017-05-27 01:03:06 +0100

    sub folder changes

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,13 +1,30 @@
 $(document).ready(function(){
 	
+	var enableExtraButtons = function(){
+		$( "#email-field-wrap" ).animate({
+			height: "50"
+		}, 150, function() {
+		});
+		$("#email-field-wrap").fadeIn();
+		$("#send-button").attr("disabled", false);
+	}
+
+	$("#address-field").on("input", function(){
+		enableExtraButtons();
+	});
+
 	$("#address-form").on("submit", function(){
 		$address = $("#address-field").val();
 		$name = $("#name-field").val();
-		
+		$postcode = $("#post-code-field").val();
+		$email = $("#email-field").val();
+
+		$message = "Name: " + $name + ". Address: " + $address + " " + $postcode + ". Email: " + $email;
+		console.log($message);
 		$.ajax({
 			url: "https://formspree.io/theo@postary.com", 
 			method: "POST",
-			data: {message: "Address sign up name ( " + $name + " )" + $address},
+			data: { message: $message },
 			dataType: "json"
 		});
 		
@@ -18,3 +35,4 @@ $(document).ready(function(){
 		return false;
 	});
 });
+
